2e4d808a90c0a51aaba5312b957c82c24d9359f3
Paul N. Ingelmo <paulingelmo@gmail.com>
2020-09-20 11:37:42 -0400

    Removing unnecessary files.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './shell/AppRouter'
 
-console.log('index.tsx')
-
 const render = () => {
   ReactDOM.render(<AppRouter />, document.getElementById('app'))
 }
